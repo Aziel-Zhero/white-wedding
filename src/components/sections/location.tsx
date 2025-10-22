35c9758343d2302e97170a257026cf789111b6b3
Zhero <bifrost.agenda@gmail.com>
2025-10-22 10:59:20 +0000

    coloca o mapa aqui que mandei Google Maps API Key não encontrada. Config

--- src/components/sections/location.tsx
+++ src/components/sections/location.tsx
@@ -1,4 +1,3 @@
-import WeddingMap from "@/components/map";
 import { MapPin } from "lucide-react";
 
 export default function LocationSection() {
@@ -19,7 +18,17 @@ export default function LocationSection() {
           </div>
         </div>
         
-        <WeddingMap />
+        <div className="aspect-video w-full rounded-lg overflow-hidden shadow-lg border">
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3675.659862228184!2d-45.476859925816456!3d-22.88902003723419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDUzJzIwLjUiUyA0NcKwMjgnMjcuNCJX!5e0!3m2!1spt-BR!2sbr!4v1761130681398!5m2!1spt-BR!2sbr" 
+            width="100%" 
+            height="100%" 
+            style={{border:0}} 
+            allowFullScreen={true}
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
 
       </div>
     </section>
