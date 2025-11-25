2c1bf3573d33947ecad70ec16b2e797c19fb9746
Zhero <bifrost.agenda@gmail.com>
2025-11-25 12:41:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1227915441-a8719",
+  "appId": "1:317229093570:web:9d1bde00f593e014af215b",
+  "apiKey": "AIzaSyBnOr916CJlw3kOghhudK53vQyP3LrXUIA",
+  "authDomain": "studio-1227915441-a8719.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "317229093570"
+};
